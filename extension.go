1394ec771dd765c42b8e31d0e7bbf342b9f081a9
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2020-06-30 22:38:48 -0700

    implement extension

--- extension.go
+++ extension.go
@@ -1,10 +1,40 @@
 package main
 
+import (
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/rlp"
+)
+
 type ExtensionNode struct {
 	Path []Nibble
 	Next Node
 }
 
+func NewExtensionNode(nibbles []Nibble, next Node) *ExtensionNode {
+	return &ExtensionNode{
+		Path: nibbles,
+		Next: next,
+	}
+}
+
 func (e ExtensionNode) Hash() []byte {
-	return nil
+	return crypto.Keccak256(e.Serialize())
+}
+
+func (e ExtensionNode) Raw() []interface{} {
+	hashes := make([]interface{}, 2)
+	hashes[0] = ToBytes(ToPrefixed(e.Path, false))
+	hashes[1] = e.Next.Raw()
+	return hashes
+}
+
+func (e ExtensionNode) Serialize() []byte {
+	raw := e.Raw()
+
+	branchRLP, err := rlp.EncodeToBytes(raw)
+	if err != nil {
+		panic(err)
+	}
+
+	return branchRLP
 }
